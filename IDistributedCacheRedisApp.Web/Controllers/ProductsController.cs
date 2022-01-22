51966015e7f37c92a2662cb6e8b99bea366d8496
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-22 21:54:13 +0300

    AddStackExchangeRedisCache() service ve IDistributedCache interface

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
