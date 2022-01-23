fc73a0822555dff77e5e37f78e8dd9f8c63410fc
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-23 18:21:13 +0300

    IDistributedCache-3 (Dosyaların cache’lenmesi)

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -13,22 +17,44 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache = distributedCache;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
 
             cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
 
-            _distributedCache.SetString("name","Berkay",cacheEntryOptions);
+            //await _distributedCache.SetStringAsync("name","Berkay",cacheEntryOptions);
 
+            Product product = new Product() {Id = 1, Name = "Kalem2", Price = 100};
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+             await _distributedCache.SetAsync("product:1",byteProduct);
+
+            // await  _distributedCache.SetStringAsync("product:1", jsonProduct, cacheEntryOptions);
 
             return View();
         }
 
         public IActionResult Show()
         {
-            string name = _distributedCache.GetString("name");
-            ViewBag.name = name;
+
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            
+            //string name = _distributedCache.GetString("name");
+
+
+            //string jsonProduct = _distributedCache.GetString("product:1");
+
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            ViewBag.name = product;
             return View();
         }
 
